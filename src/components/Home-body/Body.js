fe574506c468ea483c9ff008c1c052ee6b14943d
ryanKrumbholz <krumbholz98@gmail.com>
2020-03-20 11:11:47 -0400

    fixed some formatting issues

--- src/components/Home-body/Body.js
+++ src/components/Home-body/Body.js
@@ -9,7 +9,7 @@ const body = props => (
         </div>
         <div class="hero">
             <h1>MUSIC</h1>
-            <iframe width="60%" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1014316297&color=%23aa00fa&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+            <iframe width="100%" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1014316297&color=%23aa00fa&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
         </div>
     </div>
     );
