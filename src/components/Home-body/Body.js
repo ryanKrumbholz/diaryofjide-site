c5c5293874782f21c79a0c361f15b4fd36dd1ca0
ryanKrumbholz <krumbholz98@gmail.com>
2020-03-20 18:51:06 -0400

    Resized logo and added apple music and spotify links

--- src/components/Home-body/Body.js
+++ src/components/Home-body/Body.js
@@ -5,11 +5,15 @@ import './Body.sass';
 const body = props => (
     <div class="container">
         <div class="about">
-            <img src="https://i.imgur.com/6NjrV8M.gif"></img>   
+            <img src="https://im2.ezgif.com/tmp/ezgif-2-ca9022c3a1dd.gif"></img>   
         </div>
+        <h1>MUSIC</h1>
         <div class="hero">
-            <h1>MUSIC</h1>
             <iframe width="100%" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1014316297&color=%23aa00fa&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+            <div class="music-links">
+            <a href="https://music.apple.com/ng/artist/jide/1363234148"><img class="app-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/404px-Apple_logo_black.svg.png"></img></a>
+            <a href="https://open.spotify.com/artist/6aIwarIP5jEoZeVptga1Lq?si=9f_AE4VVRXa6RResiMU1Rg"><img class="spotify-logo" src="https://cdn4.iconfinder.com/data/icons/miu-black-social-2/60/Spotify-512.png"></img></a>
+            </div>
         </div>
     </div>
     );
